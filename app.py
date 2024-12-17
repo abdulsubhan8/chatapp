ba2e4fff1df0bde94fb6ddef01e589a50c970fba
AnasVayani <108940399+AnasVayani@users.noreply.github.com>
2024-12-17 18:19:51 +0500

    Removing prefix of env app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from threading import Lock
 # Load environment variables
 load_dotenv()
 
+
 # Get environment variables
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-FLASK_SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+AWS_ACCESS_KEY_ID = os.getenv('ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
+FLASK_SECRET_KEY = os.getenv('SECRET_KEY')
 QUEUE_URL = os.getenv('QUEUE_URL')
 
 # Flask app configuration
